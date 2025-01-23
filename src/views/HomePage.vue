7fb1f60eba4af7c9741e169fcdbc086fa5eb0a1c
root <root@BLUEBERRY.localdomain>
2025-01-22 23:22:14 -0600

    Initial commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,34 @@
+<!-- views/HomePage.vue (renamed from Home.vue) -->
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'  // Add this import
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
