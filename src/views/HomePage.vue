f71241e2c604d58e0d11bfc7a0187c055a04db2b
ekjohnson4 <ekjohnson4@crimson.ua.edu>
2025-04-28 23:29:09 -0500

    Add in product info

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -47,6 +47,24 @@
         </div>
       </div>
 
+      <div class="materials-container">
+        <div class="materials-wrapper">
+          <div
+            class="item"
+            v-for="(material, index) in materials"
+            :key="index"
+          >
+            <img src="material.imageUrl" alt="Product Image" style="width: 100px; height: auto;">
+            <div><strong>{{ material.title }}</strong></div>
+            <div>Price: {{ material.price }}</div>
+            <div v-if="material.bulkPricing">
+              Bulk ({{ material.bulkPricing.quantity }}+): {{ material.bulkPricing.price }}
+            </div>
+            <a :href="material.link" target="_blank">View Product</a>
+          </div>
+        </div>
+      </div>
+
       <div class="stats-container">
         <div class="stat-item">
           <span class="stat-label">Area:</span>
@@ -72,12 +90,12 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import HomePageMain from '../components/Home/HomePageMain.vue'
 
 const CONCRETE_COST_PER_SQFT = 6 // Cost per square foot of concrete
 
-const isOpen = ref(false)
+const isOpen = ref(true)
 const blockCount = ref(0)
 const blockSqFt = ref(4)
 const foundationThickness = ref(8) // default 8 inches
@@ -88,6 +106,18 @@ const mainMargin = computed(() => isOpen.value ? '250px' : '0px')
 
 const foundationArea = computed(() => Math.round(blockCount.value * blockSqFt.value))
 const estimatedCost = computed(() => foundationArea.value * CONCRETE_COST_PER_SQFT)
+
+const materials = ref([]);
+
+onMounted(async () => {
+  try {
+    const res = await fetch('http://localhost:3001/api/materials?url=https://www.homedepot.com/b/Building-Materials-Concrete-Cement-Masonry-Rebar-Accessories-Rebar/N-5yc1vZ1z18g8s');
+    const data = await res.json();
+    materials.value = data;
+  } catch (err) {
+    console.error('Error fetching materials:', err);
+  }
+});
 
 const toggleNav = () => {
   isOpen.value = !isOpen.value
