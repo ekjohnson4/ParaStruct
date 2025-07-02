04d57f6f7a9a4eca6665464aab048febe3e9b861
ekjohnson4 <ekjohnson4@crimson.ua.edu>
2025-07-02 17:26:54 -0500

    Heroku stuff

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, '../server/public'),
+  assetsDir: 'static',
+  publicPath: '/',
 })
