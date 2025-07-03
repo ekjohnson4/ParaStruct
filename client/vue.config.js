62607227ac9914d3c5d7827d23e718ff1345b5e7
ekjohnson4 <ekjohnson4@crimson.ua.edu>
2025-07-02 21:18:02 -0500

    Change dist to public

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, './public'),
+  outputDir: path.resolve(__dirname, '../server/public'),
   assetsDir: 'static',
   publicPath: '/',
 })
